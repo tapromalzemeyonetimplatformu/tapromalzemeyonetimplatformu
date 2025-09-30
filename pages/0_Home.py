f48f4ac5f0152fcf25dfc06fd8346b2b3ad10e1c
tapromalzemeyonetimplatformu <tapromalzemeyonetimplatformu@gmail.com>
2025-09-30 23:24:25 +0300

    Update 0_Home.py

--- pages/0_Home.py
+++ pages/0_Home.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-# ✅ Kullanıcı giriş kontrolü
-if "authenticated" not in st.session_state or not st.session_state.authenticated:
-    st.error("🔒 You must be logged in to access this page.")
-    st.stop()
-
 # Sayfa başlığı ve düzeni
 st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 
