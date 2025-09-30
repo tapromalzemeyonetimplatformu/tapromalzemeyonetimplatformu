db299611e9c362a83fa723cd6b6f5c6f2ecbd591
tapromalzemeyonetimplatformu <tapromalzemeyonetimplatformu@gmail.com>
2025-09-30 21:57:06 +0300

    Update 0_Home.py

--- pages/0_Home.py
+++ pages/0_Home.py
@@ -8,9 +8,8 @@ st.title("🏠 Welcome to TA & PRO Common Space")
 
 # Yan menüdeki seçenekler
 page = st.sidebar.radio("📁 Navigation", [
-    "COMPADDITIVE Material Selection",
-    "COMPADDITIVE Literature Reviewer",
-    "CREDIT Literature Reviewer",
+    "Material Selector",
+    "Literature Reviewer",
     "Tensile Test Library",
     "DSC Library",
     "SEM & EDS Library",
@@ -20,25 +19,22 @@ page = st.sidebar.radio("📁 Navigation", [
 
 # Seçilen menüye göre sayfaya yönlendirme
 if page == "COMPADDITIVE Material Selection":
-    st.switch_page("pages/1_COMPADDITIVE_Material_Selection.py")
+    st.switch_page("pages/1_Material_Selector.py")
 
 elif page == "COMPADDITIVE Literature Reviewer":
-    st.switch_page("pages/2_COMPADDITIVE_Literature_Reviewer.py")
-
-elif page == "CREDIT Literature Reviewer":
-    st.switch_page("pages/3_CREDIT_Literature_Reviewer.py")
+    st.switch_page("pages/2_Literature_Reviewer.py")
 
 elif page == "Tensile Test Library":
-    st.switch_page("pages/4_Tensile_Test_Library.py")
+    st.switch_page("pages/3_Tensile_Test_Library.py")
 
 elif page == "DSC Library":
-    st.switch_page("pages/5_DSC_Library.py")
+    st.switch_page("pages/4_DSC_Library.py")
 
 elif page == "SEM & EDS Library":
-    st.switch_page("pages/6_SEM_&_EDS_Library.py")
+    st.switch_page("pages/5_SEM_&_EDS_Library.py")
 
 elif page == "Fatigue Test Library":
-    st.switch_page("pages/7_Fatigue_Test_Library.py")
+    st.switch_page("pages/6_Fatigue_Test_Library.py")
 
 elif page == "Production Tracker":
-    st.switch_page("pages/8_Production_Tracker.py")
+    st.switch_page("pages/7_Production_Tracker.py")
